0aaa424e526a6caf2e2a771e5de2f895a4724b5e
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-07-08 18:27:49 +0300

    SaveStudent in Local Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CRUD_operation_Angular17';
+  title = ' Student Portal';
+  @ViewChild('ModalCreate') model: ElementRef | undefined;
+  studentObj: student = new student();
+
+
+  openModel() {
+    const model = document.getElementById("ModalCreate");
+    if (model != null) {
+      model.style.display = 'block';
+    }
+  }
+
+
+
+  closeModel() {
+    if (this.model != null) {
+      this.model.nativeElement.style.display = 'none';
+    }
+  }
+
+  SaveStudent() {
+    const isLocalPresent = localStorage.getItem("Angular17CRUD");
+    if (isLocalPresent != null) {
+      const oldArr = JSON.parse(isLocalPresent);
+      oldArr.push(this.studentObj);
+      localStorage.setItem('Angular17CRUD', JSON.stringify(oldArr));
+    }
+    else {
+      const newArr = [];
+      newArr.push(this.studentObj);
+
+      localStorage.setItem('Angular17CRUD', JSON.stringify(newArr));
+    }
+  }
+
+}
+
+
+
+export class student {
+  name: string;
+  mobileNo: string;
+  email: string;
+  city: string;
+  state: string;
+  code: string;
+  address: string;
+
+  constructor() {
+    this.name = '';
+    this.mobileNo = '';
+    this.email = '';
+    this.city = '';
+    this.state = '';
+    this.code = '';
+    this.address = '';
+  }
 }
