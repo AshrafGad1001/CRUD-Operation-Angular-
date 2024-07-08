32805fd8047b4609c089b410cec25f10e4f00713
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-07-08 20:32:29 +0300

    End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = ' Student Portal';
   @ViewChild('ModalCreate') model: ElementRef | undefined;
   studentObj: student = new student();
-
+  studentList: student[] = [];
+  ngOnInit(): void {
+    const localData = localStorage.getItem("Angular17CRUD");
+    if (localData != null) {
+      this.studentList = JSON.parse(localData);
+    }
+  }
 
   openModel() {
     const model = document.getElementById("ModalCreate");
@@ -25,31 +32,72 @@ export class AppComponent {
 
 
   closeModel() {
+    this.studentObj = new student();
     if (this.model != null) {
       this.model.nativeElement.style.display = 'none';
     }
   }
 
+  onEdit(item: student) {
+    this.studentObj = item;
+    this.openModel();
+  }
   SaveStudent() {
     const isLocalPresent = localStorage.getItem("Angular17CRUD");
     if (isLocalPresent != null) {
       const oldArr = JSON.parse(isLocalPresent);
       oldArr.push(this.studentObj);
+      this.studentObj.id = oldArr.length;
+      this.studentList = oldArr;
       localStorage.setItem('Angular17CRUD', JSON.stringify(oldArr));
     }
     else {
       const newArr = [];
       newArr.push(this.studentObj);
-
+      this.studentObj.id = 1;
+      this.studentList = newArr;
       localStorage.setItem('Angular17CRUD', JSON.stringify(newArr));
     }
+
+    this.closeModel();
   }
 
+  updateStudent() {
+    const currentStd = this.studentList.find(m => m.id === this.studentObj.id);
+
+    if (currentStd != undefined) {
+      currentStd.name = this.studentObj.name;
+      currentStd.mobileNo = this.studentObj.mobileNo;
+      currentStd.email = this.studentObj.email;
+      currentStd.code = this.studentObj.code;
+      currentStd.city = this.studentObj.city;
+      currentStd.state = this.studentObj.state;
+      currentStd.address = this.studentObj.address;
+    };
+
+    localStorage.setItem('Angular17CRUD', JSON.stringify(this.studentList));
+    this.closeModel();
+  }
+
+
+  onDelete(item: student) {
+    const IsDelete = confirm("Are You Sure");
+
+    if (IsDelete) {
+      const currentStd = this.studentList.findIndex(m => m.id === this.studentObj.id);
+      this.studentList.splice(currentStd, 1);
+      localStorage.setItem('Angular17CRUD', JSON.stringify(this.studentList));
+    }
+
+
+
+  }
 }
 
 
 
 export class student {
+  id: number;
   name: string;
   mobileNo: string;
   email: string;
@@ -59,6 +107,7 @@ export class student {
   address: string;
 
   constructor() {
+    this.id = 0;
     this.name = '';
     this.mobileNo = '';
     this.email = '';
